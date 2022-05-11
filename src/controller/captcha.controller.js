e2fd67c6a204cbe2e0055a7e07f68e4d546533ab
wuyupei <495174699@qq.com>
2022-05-11 21:21:10 +0800

    code

--- src/controller/captcha.controller.js
+++ src/controller/captcha.controller.js
@@ -2,13 +2,14 @@ const svgCaptcha = require('svg-captcha');
 
 class captchaController {
   async code(ctx, next) {
-    const captcha = svgCaptcha.create();
+    const captcha = svgCaptcha.create({
+      width: 100, //宽度
+      height: 32, //高度
+    });
     // ctx.setCookie();
     ctx.cookies.set('code', captcha.text, {
       maxAge: 60 * 1000, //设置过期时间，
       httpOnly: false, // 客户端可以访问到
-      width: 100, //宽度
-      height: 32, //高度
     });
     return (ctx.body = {
       code: captcha.data,
