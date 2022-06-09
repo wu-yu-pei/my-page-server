4375718db1840c8f2c4bb17c4f7877f7c184115d
wuyupei <495174699@qq.com>
2022-06-09 13:38:39 +0800

    fix bug

--- src/app/index.js
+++ src/app/index.js
@@ -11,23 +11,22 @@ const setupMiddleware = require('./useMiddleware');
 const setupRouter = require('./useRouter');
 const setupSocketIo = require('./useSocketIo')
 
+const app = new Koa();
+
+// setup https
+const options = {
+  key: fs.readFileSync(path.resolve('./src/config/https/7892505_wuyupei.top.key')),
+  cert: fs.readFileSync('./src/config/https/7892505_wuyupei.top.pem'),
+};
+const server = https.createServer(options, app.callback());
+
+setupMiddleware(app)
+
+setupRouter(app)
+
+setupSocketIo(server)
 
 module.exports = function (port) {
-  const app = new Koa();
-
-  // setup https
-  const options = {
-    key: fs.readFileSync(path.resolve('./src/config/https/7892505_wuyupei.top.key')),
-    cert: fs.readFileSync('./src/config/https/7892505_wuyupei.top.pem'),
-  };
-  const server = https.createServer(options, app.callback());
-
-  setupMiddleware(app)
-
-  setupRouter(app)
-
-  setupSocketIo(server)
-
   // run
   server.listen(port, () => {
     console.log('server is runing');
