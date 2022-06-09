d0e5346e50f9bd05bb4f555878082944ed770c53
wuyupei <495174699@qq.com>
2022-06-09 14:18:23 +0800

    ok

--- src/app/index.js
+++ src/app/index.js
@@ -11,22 +11,23 @@ const setupMiddleware = require('./useMiddleware');
 const setupRouter = require('./useRouter');
 const setupSocketIo = require('./useSocketIo')
 
-const app = new Koa();
-
-// setup https
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '../config/https/7892505_wuyupei.top.key')),
-  cert:fs.readFileSync(path.join(__dirname, '../config/https/7892505_wuyupei.top.pem')),
-};
-const server = https.createServer(options, app.callback());
-
-setupMiddleware(app)
-
-setupRouter(app)
-
-setupSocketIo(server)
 
 module.exports = function (port) {
+  const app = new Koa();
+
+  // setup https
+  const options = {
+    key: fs.readFileSync(path.join(__dirname, '../config/https/7892505_wuyupei.top.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../config/https/7892505_wuyupei.top.pem')),
+  };
+  const server = https.createServer(options, app.callback());
+
+  setupMiddleware(app)
+
+  setupRouter(app)
+
+  setupSocketIo(server)
+
   // run
   server.listen(port, () => {
     console.log('server is runing');
