f28e04fb825189726f684af53bc7db56b1a6f8b9
Wuyupei <495174699@qq.com>
2022-06-29 17:25:38 +0800

    map

--- src/app/useSocketIo.js
+++ src/app/useSocketIo.js
@@ -8,28 +8,38 @@ module.exports = function (server) {
     }
   });
 
-
   io.on('connect', async (socket) => {
-    // 标识
-    const name = Math.random()
-    socket.name = name
-    // redis里设置在线人数
-    await redis.hset('users', { [name]: 1 })
-    // 广播出去
-    socket.broadcast.emit('user join', await redis.hlen('users'))
+    if (socket.handshake.query.auth) {
+      const room = socket.handshake.query.auth
+      console.log('connect.....');
+      socket.join(room)
 
-
-    // 发来消息
-    socket.on('message', async (data) => {
-      await redis.rpush("mylist", JSON.stringify(data))
-      // 广播
-      socket.broadcast.emit("guangbo", data);
-    })
-    // 断开连接
-    socket.on('disconnect', async (e) => {
-      // 更新在线人数
-      await redis.hdel('users', socket.name)
+      socket.on('location', (res) => {
+        console.log(res);
+        socket.to(room).emit('location', {  res })
+      })
+    } else {
+      // 标识
+      const name = Math.random()
+      socket.name = name
+      // redis里设置在线人数
+      await redis.hset('users', { [name]: 1 })
+      // 广播出去
       socket.broadcast.emit('user join', await redis.hlen('users'))
-    });
+
+
+      // 发来消息
+      socket.on('message', async (data) => {
+        await redis.rpush("mylist", JSON.stringify(data))
+        // 广播
+        socket.broadcast.emit("guangbo", data);
+      })
+      // 断开连接
+      socket.on('disconnect', async (e) => {
+        // 更新在线人数
+        await redis.hdel('users', socket.name)
+        socket.broadcast.emit('user join', await redis.hlen('users'))
+      });
+    }
   })
 }
